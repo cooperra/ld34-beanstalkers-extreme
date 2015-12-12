5cb884f92d68e7a7d7274579138e3f3ed1772463
Allen Brower <bslashingu@gmail.com>
2015-12-12 02:04:34 -0500

    Fixed some stuff

--- Assets/Scripts/Beanstalk/TrashGenerator.cs
+++ Assets/Scripts/Beanstalk/TrashGenerator.cs
@@ -12,7 +12,8 @@ public class TrashGenerator : MainBehaviour {
 
 		if(GameTime >= _lastTime + Rate){
 
-			Instantiate(TrashObject, new Vector3(Random.Range(-3, 3), Player.transform.position.y + 10, 0), Quaternion.identity);
+			GameObject obj = Instantiate(TrashObject, new Vector3(Random.Range(-3, 3), Player.transform.position.y + 10, 0), Quaternion.identity) as GameObject;
+			obj.transform.parent = transform.parent;
 
 			_lastTime = GameTime;
 		}
