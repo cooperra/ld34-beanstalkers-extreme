859899168030b7cf82c1ca7a7059d461f377665c
Allen Brower <bslashingu@gmail.com>
2015-12-12 18:31:05 -0500

    Parallax scrolling, SON

--- Assets/Scripts/Beanstalk/ParallaxManager.cs
+++ Assets/Scripts/Beanstalk/ParallaxManager.cs
@@ -7,9 +7,17 @@ public class ParallaxManager : MonoBehaviour {
 	public Transform FarBG;
 	public float StartY = 5;
 
+	public Transform[] Layers;
+	public float ParallaxFactor = .5f;
+	public float ParallaxStartY = 5;
+
 	void Update(){
 
 		FarBG.transform.localPosition = new Vector3(0, StartY + (transform.position.y / ScrollSpeed), 15);
+
+		for(int i = 0; i < Layers.Length; i++){
+			Layers[i].transform.localPosition = new Vector3(0, (transform.position.y / (ParallaxFactor * (-i - 1))), 15);
+		}
 
 	}
 
