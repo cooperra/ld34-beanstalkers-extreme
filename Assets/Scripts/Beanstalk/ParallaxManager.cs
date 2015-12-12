1abce9cae24a23ab636bdf6c6cd7dbb3c4bc71dc
Allen Brower <bslashingu@gmail.com>
2015-12-12 18:05:49 -0500

    Added in the background

---
+++ Assets/Scripts/Beanstalk/ParallaxManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxManager : MonoBehaviour {
+
+	public float ScrollSpeed = .5f;
+	public Transform FarBG;
+	public float StartY = 5;
+
+	void Update(){
+
+		FarBG.transform.localPosition = new Vector3(0, StartY + (transform.position.y / ScrollSpeed), 15);
+
+	}
+
+}
