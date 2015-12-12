03caf20e09bbe7e0b63fcd7b1211cf3e5513fe4b
Allen Brower <bslashingu@gmail.com>
2015-12-12 00:58:26 -0500

    Made the trash spawn dynamically

---
+++ Assets/Scripts/Beanstalk/TrashGenerator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashGenerator : MainBehaviour {
+
+	public Transform Player;
+	public GameObject TrashObject;
+	public float Rate = 5.0f;
+	private float _lastTime = 0.0f;
+
+	protected override void GameUpdate(){
+
+		if(GameTime >= _lastTime + Rate){
+
+			Instantiate(TrashObject, new Vector3(Random.Range(-3, 3), Player.transform.position.y + 10, 0), Quaternion.identity);
+
+			_lastTime = GameTime;
+		}
+
+	}
+
+}
