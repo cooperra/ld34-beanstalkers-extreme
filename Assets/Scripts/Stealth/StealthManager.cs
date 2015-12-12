a93347dec6cbf36a0daed9f66fb010f65ff4f1e7
Allen Brower <bslashingu@gmail.com>
2015-12-12 15:27:16 -0500

    Added in the giant that peeks at the player.

--- Assets/Scripts/Stealth/StealthManager.cs
+++ Assets/Scripts/Stealth/StealthManager.cs
@@ -12,25 +12,36 @@ public class StealthManager : MainBehaviour {
 	public StealthPlayer Player;
 
 	public GameObject[] DebugLights;
+	public Transform Giant;
+	public Transform GiantMoveTo;
+
 
 	private int CurrentLight = 0;
 	private float _currentStealthSpeed = 0.0f;
 	private float _lastStealth = 0.0f;
 	private float _randomTime = 0.0f;
 	private float _timeModifier = 0.05f;
+	private Vector3 _giantInitialPos;
+
 
 	void Awake(){
 		if(Instance == null)
 			Instance = this;
 
 		_currentStealthSpeed = StealthSpeed;
+		_giantInitialPos = Giant.localPosition;
 	}
 
 	protected override void GameUpdate(){
 
-		if(CurrentLight == 0)
+		if(CurrentLight == 0){
+			Giant.transform.localPosition = Vector3.MoveTowards(Giant.localPosition, GiantMoveTo.localPosition, 35.0f * Time.deltaTime);
 			if(!Player.Stealthed && GameTime >= _lastStealth + StealthForgiveness)	
 				DetectedPlayer();
+		}
+		else{
+			Giant.transform.localPosition = Vector3.MoveTowards(Giant.localPosition, _giantInitialPos, 25.0f * Time.deltaTime);
+		}
 
 		if(GameTime >= _lastStealth + _randomTime){
 
