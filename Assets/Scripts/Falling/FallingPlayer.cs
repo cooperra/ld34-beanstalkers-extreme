f9fb3ec03db42198659a067679e28e7bbb6c4fab
Robbie Cooper <cooperra@users.noreply.github.com>
2015-12-13 03:22:39 -0500

    Add falling acceleration. Increase falling speed.

--- Assets/Scripts/Falling/FallingPlayer.cs
+++ Assets/Scripts/Falling/FallingPlayer.cs
@@ -3,15 +3,17 @@ using System.Collections;
 
 public class FallingPlayer : MainBehaviour {
 
-	public float FallSpeed = 2.0f;
+	public float FallAcceleration = 1.0f; // per frame?
+	public float FallMaxSpeed = 2.0f;//3
 
-	public float HorizontalSpeed = 2.0f;
-	public float HorizontalMaxSpeed = 4.0f;
+	public float HorizontalSpeed = 2.0f;//5
+	public float HorizontalMaxSpeed = 4.0f;//2
 
-	public float HorizontalLimits = 3.0f;
+	public float HorizontalLimits = 3.0f;//3
 
 
 	private float _xVelocity = 0.0f;
+	private float _yVelocity = 0.0f;
 	private float _input = 0.0f;
 
 
@@ -28,9 +30,12 @@ public class FallingPlayer : MainBehaviour {
 			_xVelocity =Mathf.Lerp(_xVelocity, 0, 4.5f * Time.deltaTime);
 		}
 
+		_yVelocity -= FallAcceleration;
+		_yVelocity = Mathf.Clamp(_yVelocity, -FallMaxSpeed, 0);
+
 		_xVelocity = Mathf.Clamp(_xVelocity, -HorizontalMaxSpeed, HorizontalMaxSpeed);
 
-		transform.position = new Vector3(transform.position.x + (_xVelocity * Time.deltaTime), transform.position.y - (FallSpeed * Time.deltaTime), 0);
+		transform.position = new Vector3(transform.position.x + (_xVelocity * Time.deltaTime), transform.position.y + (_yVelocity * Time.deltaTime), 0);
 
 		transform.position = new Vector2(Mathf.Clamp(transform.position.x, -HorizontalLimits, HorizontalLimits), transform.position.y);
 
