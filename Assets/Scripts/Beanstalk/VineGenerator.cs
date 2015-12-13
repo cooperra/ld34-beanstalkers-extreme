f5e1b7df651ed35b1b21c0fc7fd0f6d462c2c721
Allen Brower <bslashingu@gmail.com>
2015-12-12 22:08:53 -0500

    Transformed StealthManager into a MinigameBehavior!!!!! A bunch of crap[

--- Assets/Scripts/Beanstalk/VineGenerator.cs
+++ Assets/Scripts/Beanstalk/VineGenerator.cs
@@ -22,11 +22,12 @@ public class VineGenerator : MainBehaviour {
 			_points++;
 			_line.SetVertexCount(_points);
 			//if(_points - 1 == 0)
-			_line.SetPosition(_points - 1, VinePoint.position);
+			
 			//else
 				//_line.SetPosition(_points - 1, VinePoint.position - new Vector3(0, y, 0));
 			_lastCreated = GameTime;
 		}
+		_line.SetPosition(_points - 1, VinePoint.position);
 		GetComponent<LineRenderer>().materials[0].mainTextureScale = new Vector2(GetComponent<LineRenderer>().materials[0].mainTextureScale.x + (MaterialGrowthRate * Time.deltaTime), GetComponent<LineRenderer>().materials[0].mainTextureScale.y);
 
 	}
