9c3d2cc1e18f7d4a875963360aa4b7588774ff64
Allen Brower <bslashingu@gmail.com>
2015-12-12 19:28:55 -0500

    Started the falling minigame

---
+++ Assets/Scripts/Falling/FallingManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingManager : MainBehaviour {
+
+	public GameObject[] ObjectsToDisable;
+	public PlayerCamera PlayerCam;
+	public Vector3 NewOffset = new Vector3(0, -2, -10);
+	public Transform Player;
+	public Transform OldPlayer;	// The player from the Beanstalk minigame
+	public Transform Ground;
+	public float EndTrigger = 20f;
+
+	void Start(){
+		PlayerCam.follow = Player;
+		Player.transform.position = OldPlayer.transform.position;
+		PlayerCam.offset = NewOffset;
+
+		
+
+	}
+
+	protected override void GameUpdate(){
+		if(Vector3.Distance(Player.position, Ground.position) <= EndTrigger)
+			Debug.Log("End Minigame");
+	}
+	
+}
---
+++ Assets/Scripts/Falling/FallingPlayer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlayer : MonoBehaviour {
+
+	public float FallSpeed = 2.0f;
+
+	void Update(){
+
+		transform.position = new Vector3(0, transform.position.y - (FallSpeed * Time.deltaTime), 0);
+
+	}
+
+}
