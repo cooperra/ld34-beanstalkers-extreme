5743394006787ba53fae5bc8a36e9044575acf1f
Robbie Cooper <cooperra@users.noreply.github.com>
2015-12-13 18:26:53 -0500

    Add losing consequence to stealth. Keep track of babby.

--- Assets/Scripts/Stealth/StealthMinigame.cs
+++ Assets/Scripts/Stealth/StealthMinigame.cs
@@ -134,6 +134,7 @@ public class StealthMinigame : MinigameBehavior {
 
 	void Win() {
 		Debug.Log("YOU WIN");
+		StateManager.Instance.HasBabby = true;
 		ProceedNextGame();
 	}
 
@@ -148,6 +149,9 @@ public class StealthMinigame : MinigameBehavior {
 			}
 			// Change giant sprite
 			Giant.GetComponent<SpriteRenderer>().sprite = AngryGiantSprite;
+			// Set no babby
+			StateManager.Instance.HasBabby = false;
+			Invoke("ProceedNextGame", 1.5f)
 			_hasLost = true;
 		}
 
