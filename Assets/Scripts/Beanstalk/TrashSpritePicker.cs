357e2e1034340423bfd9c2e61e84ef4014d797fe
Allen Brower <bslashingu@gmail.com>
2015-12-13 00:32:13 -0500

    Added in the can, man Random trash sprite picker

---
+++ Assets/Scripts/Beanstalk/TrashSpritePicker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashSpritePicker : MonoBehaviour {
+
+	public Sprite[] RandomSprites;
+
+	void Start(){
+		int random = Random.Range(0, RandomSprites.Length);
+		GetComponent<SpriteRenderer>().sprite = RandomSprites[random];
+	}
+
+}
