59a5faa9da910505527a1fe0952b0594bd0ad0cb
Robbie Cooper <cooperra@users.noreply.github.com>
2015-12-13 04:01:53 -0500

    Add near-miss detector to trash in preparation for wooshing sounds!

---
+++ Assets/Scripts/Beanstalk/TrashWoosh.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashWoosh : MonoBehaviour {
+
+	// // Use this for initialization
+	// void Start () {
+	
+	// }
+	
+	// // Update is called once per frame
+	// void Update () {
+	
+	// }
+
+	void OnTriggerEnter2D(Collider2D collider){
+
+		if(collider.name == "Player"){
+			Woosh();
+		}
+
+	}
+
+	private void Woosh() {
+		// Do audio
+		Debug.Log("Woosh!");
+	}
+}
