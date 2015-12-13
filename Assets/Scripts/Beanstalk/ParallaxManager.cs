d258edd93d213dec5e637f12a7b3f9ff0e970b7e
Allen Brower <bslashingu@gmail.com>
2015-12-12 23:28:55 -0500

    Foreground parallax

--- Assets/Scripts/Beanstalk/ParallaxManager.cs
+++ Assets/Scripts/Beanstalk/ParallaxManager.cs
@@ -11,12 +11,20 @@ public class ParallaxManager : MonoBehaviour {
 	public float ParallaxFactor = .5f;
 	public float ParallaxStartY = 5;
 
+	public Transform[] ForegroundLayer;
+	public float ForegroundFactor = 5f;
+	public float ForgroundStartY = 50.0f;
+
 	void Update(){
 
 		FarBG.transform.localPosition = new Vector3(0, StartY + (transform.position.y / ScrollSpeed), 15);
 
 		for(int i = 0; i < Layers.Length; i++){
 			Layers[i].transform.localPosition = new Vector3(0, (transform.position.y / (ParallaxFactor * (-i - 1))), 15);
+		}
+
+		for(int i = 0; i < ForegroundLayer.Length; i++){
+			ForegroundLayer[i].transform.localPosition = new Vector3(0, ForgroundStartY + (transform.position.y / (ForegroundFactor * (-i - 1))), 15);
 		}
 
 	}
