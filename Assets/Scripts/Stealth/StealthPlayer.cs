60687cc5dce057941bfdbc73c9b09adf5ea300bc
Robbie Cooper <cooperra@users.noreply.github.com>
2015-12-14 18:48:09 -0500

    Add stealth sprite-swap logic

--- Assets/Scripts/Stealth/StealthPlayer.cs
+++ Assets/Scripts/Stealth/StealthPlayer.cs
@@ -7,19 +7,43 @@ public class StealthPlayer : MainBehaviour{
 	public bool Stealthed = false;
 	private float _input = 0.0f;
 
+	public Sprite StealthSprite;
+	private Sprite _nonStealthedSprite;
+
 	protected override void GameUpdate(){
 
 		_input = PlayerInput.Instance.UserInput;
 
 		if(_input == 1){
 			transform.position = new Vector2(transform.position.x + (MovementSpeed * Time.deltaTime), transform.position.y);
-			Stealthed = false;
+			NoStealth();
 		}
 		else if(_input == -1)
-			Stealthed = true;
+			YesStealth();
 		else
-			Stealthed = false;
+			NoStealth();
 
+	}
+
+	private void NoStealth() {
+		if (Stealthed) {
+			Stealthed = false;
+			// Unset stealth sprite
+			if (StealthSprite != null) {
+				GetComponent<SpriteRenderer>().sprite = _nonStealthedSprite;
+			}
+		}
+	}
+
+	private void YesStealth() {
+		if (!Stealthed) {
+			Stealthed = true;
+			// Set stealth sprite
+			if (StealthSprite != null) {
+				_nonStealthedSprite = GetComponent<SpriteRenderer>().sprite;
+				GetComponent<SpriteRenderer>().sprite = StealthSprite;
+			}
+		}
 	}
 
 }
