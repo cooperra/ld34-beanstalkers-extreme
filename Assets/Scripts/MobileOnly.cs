911a7efccb305f487dfa487fedc2b4d44367f106
Allen Brower <bslashingu@gmail.com>
2015-12-14 19:45:42 -0500

    Ton more stuff

---
+++ Assets/Scripts/MobileOnly.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileOnly : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		if(Application.isMobilePlatform)
+			gameObject.SetActive(false);
+	
+	}
+}
